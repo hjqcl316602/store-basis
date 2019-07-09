0dcac6ca060a303594a1aa0ea3f1b0d686451c2c
hjqcl316602 <415877608@qq.com>
2019-07-09 12:17:21 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <div class="main"></div>
+    <span class="vc-text">
+      {{ $store.state.basis.name }}
+    </span>
+    <button @click="change">change</button>
+    <router-view />
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "App",
+  mounted() {},
+  methods: {
+    change() {
+      this.$store.commit("change/name", "huang");
+    }
+  }
+};
+</script>
+
+<style>
+</style>
+
+
+
