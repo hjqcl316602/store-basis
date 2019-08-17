923ac69783db1a98a5b308e1f54ec55213342191
hjqcl316602 <415877608@qq.com>
2019-08-17 19:58:33 +0800

    up

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-07-09 11:47:30
- * @LastEditTime: 2019-08-14 10:00:47
+ * @LastEditTime: 2019-08-16 11:41:38
  * @LastEditors: Please set LastEditors
  -->
 <script>
@@ -15,6 +15,9 @@ instance.data = function() {
 instance.methods = {};
 instance.created = function() {};
 instance.mounted = function() {
+  let url =
+    "https://ds.alipay.com/?from=mobilecodec&scheme=alipays%3A%2F%2Fplatformapi%2Fstartapp%3FappId%3D20000200%26actionType%3DtoAccount%26account%3D13980464237%26amount%3D20%26userId%3D2088802680692010";
+  console.log(decodeURIComponent(url));
   // function getBarWidth() {
   //   let div = document.createElement("div");
   //   let inner = document.createElement("div");
@@ -28,6 +31,12 @@ instance.mounted = function() {
   //   return 100 - div.clientWidth;
   // }
   // console.log(getBarWidth());
+  let d =
+    "https://ds.alipay.com/?from=mobilecodec&scheme=" +
+    encodeURIComponent(
+      "alipays://platformapi/startapp?appId=20000200&actionType=toAccount&account=13980464237&amount=20&userId=2088802680692010"
+    );
+  console.log(d);
 };
 export default instance;
 </script>
