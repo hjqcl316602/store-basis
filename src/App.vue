0382cfdf79dc60217297cded683d6f16d700b0c2
hjqcl316602 <415877608@qq.com>
2019-09-09 18:22:46 +0800

    ceshi

--- src/App.vue
+++ src/App.vue
@@ -6,24 +6,28 @@
  * @LastEditors: Please set LastEditors
  -->
 <script>
-import elementResizeDetectorMaker from "element-resize-detector";
-const instance = {};
-instance.name = "";
-instance.props = {};
-instance.data = function() {
-  return {};
-};
-instance.methods = {};
-instance.created = function() {};
-instance.mounted = function() {};
-export default instance;
+	import elementResizeDetectorMaker from "element-resize-detector";
+	const instance = {};
+	instance.name = "";
+	instance.props = {};
+	instance.data = function() {
+		return {};
+	};
+	instance.methods = {};
+	instance.created = function() {};
+	instance.mounted = function() {};
+	export default instance;
 </script>
 
 <template>
-  <div class="">
-    <router-view />
-  </div>
+	<div class="">
+
+		<div class="vui-text--bold">
+
+		</div>
+		<router-view />
+	</div>
 </template>
 
-<style   scoped>
+<style scoped>
 </style>
